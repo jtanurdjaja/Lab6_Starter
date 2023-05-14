b6274d0a7f2b8f317b93b7c6945821aa3ce11046
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2023-05-14 01:24:53 -0700

    Completed A1-A9

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,10 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  if (localStorage.getItem('recipes') == null) {
+    return [];
+  }
+  return JSON.parse(localStorage.getItem('recipes'));
 }
 
 /**
