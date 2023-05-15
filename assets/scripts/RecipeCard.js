2f08e9657ffeb1331fc3f50ab4fbb03fe960f7df
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2023-05-14 17:12:02 -0700

    Completed Lab 6

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,7 +10,7 @@ class RecipeCard extends HTMLElement {
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     const shadowDom = this.attachShadow({mode: 'open'});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    const article = document.createElement('article');
+    const articleEle = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     const styleEle = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
@@ -91,7 +91,7 @@ class RecipeCard extends HTMLElement {
     }`;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
     shadowDom.append(styleEle);
-    shadowDom.append(article);
+    shadowDom.append(articleEle);
   }
 
   /**
@@ -120,25 +120,25 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    const article = shadowDom.querySelector('article');
+    const article = this.shadowRoot.querySelector('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (template strings) and element.innerHTML for this.
     const articleContent = `<img src="${data.imgSrc}"
-    alt="${imgAlt}">
+    alt="${data.imgAlt}">
   <p class="title">
-    <a href="${titleLnk}">${titleTxt}</a>
+    <a href="${data.titleLnk}">${data.titleTxt}</a>
   </p>
-  <p class="organization">${organization}</p>
+  <p class="organization">${data.organization}</p>
   <div class="rating">
-    <span>${rating}</span>
-    <img src="${numRatings}" alt="${numRatings}">
-    <span>(500)</span>
+    <span>(${data.rating})</span>
+    <img src="assets/images/icons/${data.rating}-star.svg" alt="${data.rating}">
+    <span>(${data.numRatings})</span>
   </div>
-  <time>${lengthTime}</time>
+  <time>${data.lengthTime}</time>
   <p class="ingredients">
-    ${ingredients}
+    ${data.ingredients}
   </p>`;
   article.innerHTML = articleContent;
   }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -39,10 +39,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const mainEle = document.querySelector("main");
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let rec of recipes) {
+    const recipeCard = document.createElement('recipe-card');
+    recipeCard.data = rec;
+    mainEle.append(recipeCard);
+  }
 }
 
 /**
@@ -55,6 +61,8 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  const stringRecipes = JSON.stringify(recipes);
+  localStorage.setItem("recipes", stringRecipes);
 }
 
 /**
@@ -62,28 +70,44 @@ function saveRecipesToStorage(recipes) {
  * <button>.
  */
 function initFormHandler() {
+  const mainEle = document.querySelector('main');
 
   // B2. TODO - Get a reference to the <form> element
-  
+  const formEle = document.querySelector('form');
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
-
-  // Steps B4-B9 will occur inside the event listener from step B3
-  // B4. TODO - Create a new FormData object from the <form> element reference above
-  // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
-  //            make this easier to read), and then extract the keys and corresponding
-  //            values from the FormData object and insert them into recipeObject
-  // B6. TODO - Create a new <recipe-card> element
-  // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-  // B8. TODO - Append this new <recipe-card> to <main>
-  // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-  //            then save the recipes array back to localStorage
+  formEle.addEventListener('submit', function() {
+    // Steps B4-B9 will occur inside the event listener from step B3
+    // B4. TODO - Create a new FormData object from the <form> element reference above
+    const newFormData = new FormData(formEle);
+    // B5. TODO - Create an empty object (I'll refer to this object as recipeObject to
+    //            make this easier to read), and then extract the keys and corresponding
+    //            values from the FormData object and insert them into recipeObject
+    const recipeObject = {};
+    for(let key of newFormData.keys()) {
+      recipeObject[key] = newFormData.get(key);
+    }
+    // B6. TODO - Create a new <recipe-card> element
+    const newRecipeCard = document.createElement('recipe-card');
+    // B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+    newRecipeCard.data = recipeObject;
+    // B8. TODO - Append this new <recipe-card> to <main>
+    mainEle.append(newRecipeCard);
+    // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+    //            then save the recipes array back to localStorage
+    const localRecipes = getRecipesFromStorage();
+    localRecipes.push(recipeObject);
+    saveRecipesToStorage(localRecipes);
+  });
 
   // B10. TODO - Get a reference to the "Clear Local Storage" button
+  const clearButton = document.getElementsByClassName('danger')[0];
   // B11. TODO - Add a click event listener to clear local storage button
-  
-  // Steps B12 & B13 will occur inside the event listener from step B11
-  // B12. TODO - Clear the local storage
-  // B13. TODO - Delete the contents of <main>
-
+  clearButton.addEventListener('click', function(){
+    // Steps B12 & B13 will occur inside the event listener from step B11
+    // B12. TODO - Clear the local storage
+    localStorage.clear();
+    // B13. TODO - Delete the contents of <main>
+    mainEle.innerHTML = '';
+  });
 }
